61f286263821141113a9cea6c63733201b6de0d7
spsandwichman <73349893+spsandwichman@users.noreply.github.com>
2024-08-03 11:05:10 -0400

    ?

--- lang.lisp
+++ lang.lisp
@@ -1,19 +1,5 @@
-(import t )
+(var mutable-variable 2)
 
-(fun indexof (l item) (
-    cond (
-        ((is-nil l)         nil)
-        ((eq (first l) item)  0)
-        (true               (+ 1 (indexof (rest l) item))))))
-
-(fun nth (i l) (
-    cond (
-        ((is-nil l) nil)
-        ((eq i 0)   (car l))
-        (true       (nth (- i 1) (cdr l))))))
-
-(fun set-elem (i l item) (
-    cond (
-        ((is-nil l) nil)
-        ((eq i 0)   (set (car l) item))
-        (true       (set-elem (- index 1) (rest l) item)))))
+(fn x+10 (x) (
+    + 10 x
+))
